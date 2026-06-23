28e091836aa26483185e7c57e24ebe51fd55df8f
👻 Kunal Singh <kunal.s@reward360.co>
2026-06-23 12:07:25 +0530

    dd

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -134,13 +134,6 @@ export default async function RootLayout({ children }: Props) {
     >
       <head>
         <meta name="theme-color" content="#0f326a" />
-        <link
-          rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
-          integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
-          crossOrigin="anonymous"
-          referrerPolicy="no-referrer"
-        />
       </head>
 
       <AuthProvider
